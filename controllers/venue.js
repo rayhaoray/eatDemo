0c8fd1bb16355b7537273af0abeb81c478f13540
Zhenhao <rayhaoray@gmail.com>
2014-10-30 00:55:57 -0400

    Added Post tip

--- controllers/venue.js
+++ controllers/venue.js
@@ -6,17 +6,15 @@ exports.venue = function(req, res) {
 	});
 };
 
-exports.queryVenues = function(req, res){
-	var venue = new Venue({
-		name: "Hotdog",
-		tip: "Nice"
-	});
-	venue.save(function (err, venue){
-		if(err) return console.error(err);
-		venue.speak();
-	});
-	Venue.find(function (err, venues){
-		if(err) return console.error(err);
-		console.log(venues);
+exports.postVenue = function(req, res){
+	Venue.find({name: 'Hotdog'}, function (err, venues){
+		var venue = venues[0];
+		venue.tip = req.body.tip;
+		console.log(venue.tip)	;
+		venue.save(function (err, venue) {
+			if(err) return console.log("error");
+			req.flash('success', { msg: 'Venue Added.' });
+			res.redirect('/venue');
+		});
 	});
 };
