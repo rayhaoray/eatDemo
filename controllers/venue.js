7a6fd01ffa8cea211281384762e3d16c6da1c066
Zhenhao <rayhaoray@gmail.com>
2014-11-01 02:33:18 -0400

    Added like unlike button

--- controllers/venue.js
+++ controllers/venue.js
@@ -1,19 +1,24 @@
 var Venue = require('../models/Venue');
 
 exports.venue = function(req, res) {
-	Venue.find({name: 'Hotdog'}, function (err, docs){
-		res.render('venue', {venues: docs});
+	Venue.findOne({name: 'Hotdog'}, function (err, currentVenue){
+		console.log(currentVenue);
+		res.render('venue', {venue: currentVenue});
 	});
 };
 
 exports.postVenue = function(req, res){
 	Venue.find({name: 'Hotdog'}, function (err, venues){
 		var venue = venues[0];
-		venue.tip = req.body.tip;
-		console.log(venue.tip)	;
+		if(req.body.tip != '')
+			venue.tip.push(req.body.tip);
+		if(req.body.like == 'on')
+			venue.like++;
+		if(req.body.unlike == 'on')
+			venue.unlike++;
 		venue.save(function (err, venue) {
 			if(err) return console.log("error");
-			req.flash('success', { msg: 'Venue Added.' });
+			req.flash('success', { msg: 'Venue Updated.' });
 			res.redirect('/venue');
 		});
 	});
--- models/Venue.js
+++ models/Venue.js
@@ -2,12 +2,15 @@ var mongoose = require('mongoose');
 
 var venueSchema = new mongoose.Schema({
 	name: String,
-	tip: String
-});
+	tip: [],
+	like: Number,
+	unlike: Number,
+	score: Number,
+	visit: Number
+})
 
 venueSchema.methods.speak = function(){
 	var greet = this.name ? 'Venue name: ' + this.name : 'No name'
-};
-
+}
 module.exports = mongoose.model('Venue', venueSchema);
 
