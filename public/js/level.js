2a1ab68056ec3d0ec002f02134cb5ceb5eec497d
Bohan Xu <bxu@sessionm.com>
2014-11-14 21:05:45 -0500

    add separator between options

--- public/js/level.js
+++ public/js/level.js
@@ -1,8 +1,6 @@
 (function() {
   function viewModel() {
     var self = this
-    //this.phones = ko.observableArray();
-
     self.level1 = ko.observableArray();
     self.level2 = ko.observableArray();
     self.level3 = ko.observableArray();
@@ -15,19 +13,23 @@
       self.levelSelection1(this.key())
       console.log('this',this)
     }
+
     self.level2Click = function() {
       self.levelSelection2(this.key())
       console.log('this',this)
     }
+
     self.level3Click = function() {
       self.levelSelection3(this.key())
       console.log('this',this)
     }
+
     self.showSelect2 = function() {
       console.log(self.levelSelection1())
       console.log(self.levelSelection1() !== 'select one')
       return self.levelSelection1 !== 'select one'
     }
+
     self.showSelect3 = function() {
       return self.levelSelection1 !== 'select one' && self.levelSelection1 !== 'select one'
     }
@@ -38,7 +40,7 @@
 
   function optionModel(k, v) {
     this.key = ko.observable(k)
-    this.value = ko.observable(v)
+    this.hasNext = ko.observable(v)
   }
 
   vm.levelSelection1.subscribe(function (newVal) {
@@ -46,7 +48,6 @@
     var l2 = selections[newVal]
     var keys = []
     for (var k in l2) {
-      //keys.push(k)
       keys.push(new optionModel(k, k))
     }
     vm.level2(keys)
@@ -57,8 +58,6 @@
     var l3 = selections[vm.levelSelection1()][newVal]
     var keys = []
     for (var k in l3) {
-      //console.log('k', k)
-      //keys.push(k)
       keys.push(new optionModel(k, k))
     }
     vm.level3(keys)
@@ -78,8 +77,9 @@
       selections = data
       var keys = []
       for (var k in data) {
-        keys.push(new optionModel(k, k))
+        keys.push(new optionModel(k, true))
       }
+      keys[keys.length - 1].hasNext(false)
       vm.level1(keys)
     }
   })
@@ -87,60 +87,3 @@
   ko.applyBindings(vm);
 
 })()
-
-//var example2 = new viewmodel();
-//
-//ko.applyBindings(example2, document.getElementById('cascading-dropdown'));
-
-//$('#cascading-dropdown').cascadingDropdown({
-//    selectBoxes: [
-//    {
-//        selector: '.step1',
-//        source: [{ label: '4.0"', value: 4 }, { label: '4.3"', value: 4.3 }, { label: '4.7"', value: 4.7 }, { label: '5.0"', value: 5 }]
-//    },
-//    {
-//        selector: '.step2',
-//        requires: ['.step1'],
-//        source: function(request, response) {
-//            $.getJSON('/api/resolutions', request, function(data) {
-//                var selectOnlyOption = data.length <= 1;
-//                response($.map(data, function(item, index) {
-//                    return {
-//                        label: item + 'p',
-//                        value: item,
-//                        selected: selectOnlyOption
-//                    };
-//                }));
-//            });
-//        }
-//    },
-//    {
-//        selector: '.step3',
-//        requires: ['.step1', '.step2'],
-//        requireAll: true,
-//        source: function(request, response) {
-//            $.getJSON('/api/storages', request, function(data) {
-//                response($.map(data, function(item, index) {
-//                    return {
-//                        label: item + ' GB',
-//                        value: item,
-//                        selected: index == 0
-//                    };
-//                }));
-//            });
-//        },
-//        onChange: function(event, value, requiredValues, requirementsMet) {
-//            if(!requirementsMet) return;
-//
-//            example2.loading(true);
-//
-//            var ajaxData = requiredValues;
-//            ajaxData[this.el.attr('name')] = value;
-//            $.getJSON('/api/phones', ajaxData, function(data) {
-//                example2.phones(data);
-//                example2.loading(false);
-//            });
-//        }
-//    }
-//    ]
-//});
