6a3d9b9bd611e63df9b2119efaffa5e057c9c01f
Zhenhao <rayhaoray@gmail.com>
2014-11-19 00:40:04 -0500

    Updated jade to extends from layout.jade

--- controllers/contact.js
+++ controllers/contact.js
@@ -42,8 +42,8 @@ exports.postContact = function(req, res) {
   var from = req.body.email;
   var name = req.body.name;
   var body = req.body.message;
-  var to = 'your@email.com';
-  var subject = 'Contact Form | Hackathon Starter';
+  var to = 'wang.junju@husky.neu.edu';
+  var subject = 'Contact Me | Eat In NEU';
 
   var mailOptions = {
     to: to,
