bca8f246cec48cc7197f4d7279bb519c3dee93a0
Bohan Xu <bxu@sessionm.com>
2014-12-02 23:45:07 -0500

    load venue in direction controller

--- controllers/direction.js
+++ controllers/direction.js
@@ -1,5 +1,11 @@
+var Venue = require('../models/Venue')
+
 exports.getDirection = function(req, res) {
-      res.render('direction', {
-              title: 'Direction'
-            });
+  var venue_id = req.params['venue_id']
+  Venue.findById(venue_id, function(err, venue) {
+    res.render('direction', {
+      title: 'Direction',
+      address: venue.address
+    });
+  })
 };
