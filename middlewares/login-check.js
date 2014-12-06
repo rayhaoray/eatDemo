7aaa2dca5ae422540cc6d0bef0251691d0fec3c4
Bohan Xu <bxu@sessionm.com>
2014-12-06 00:18:57 -0500

    direct back to where come from

--- middlewares/login-check.js
+++ middlewares/login-check.js
@@ -3,14 +3,17 @@ var User = require('../models/User')
 module.exports = function () {
   return function(req, res, next) {
     var user = req.user
-    console.log(user)
     if (user) {
       User.findById(user._id, function(err, foundUser) {
         if (foundUser) {
           return next()
+        } else {
         }
       })
+    } else {
+      var url = req.originalUrl
+      req.session.returnTo = url
+      res.redirect('/login')
     }
-    res.redirect('/login')
   }
 }
